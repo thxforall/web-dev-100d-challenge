fe0da2400e571a8032e99d04207f6808ed6ae44c
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-04 01:13:56 +0900

    43 Day Challenge

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
 
 const players = [
   {
@@ -16,6 +24,11 @@ const playerConfigOverlayElement =
 const backdropElement = document.getElementsByClassName('backdrop')[0];
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementsByClassName('config-errors')[0];
+const gameAreaElement = document.getElementsByClassName('active-game')[0];
+const activePlayerNameElement =
+  document.getElementsByClassName('active-player-name')[0];
+// const gameFieldElements = document.querySelectorAll('.game-board li');
+const gameBoardElement = document.getElementsByClassName('game-board')[0];
 
 const editPlayer1BtnElement =
   document.getElementsByClassName('edit-player-1-btn')[0];
@@ -23,6 +36,8 @@ const editPlayer2BtnElement =
   document.getElementsByClassName('edit-player-2-btn')[0];
 const cancleConfigBtnElement =
   document.getElementsByClassName('cancel-config-btn')[0];
+const startNewGameBtnElement =
+  document.getElementsByClassName('start-game-btn')[0];
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -31,3 +46,11 @@ cancleConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener('click', selectGameField);
+// }
+
+gameBoardElement.addEventListener('click', selectGameField);
---
+++ scripts/game.js
@@ -0,0 +1,89 @@
+function startNewGame() {
+  if (!players[0].name || !players[1].name) {
+    alert('Please set custom player names for both players!!');
+    return;
+  }
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  if (event.target.tagName !== 'LI') {
+    return;
+  }
+
+  const selectedField = event.target;
+
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please select an empty field');
+    return;
+  }
+
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add('disabled');
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  const winnerID = checkForGameOver();
+
+  currentRound++;
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checkung the rows for eqaulity
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // Checkung the colums for eqaulity
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
+}
