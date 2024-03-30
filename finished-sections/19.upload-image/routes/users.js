53c5c31b620db7cae0a23109878402c85e4f2816
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-03-31 05:02:30 +0900

    68

--- finished-sections/19.upload-image/routes/users.js
+++ finished-sections/19.upload-image/routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import multer from 'multer';
 
+import { db } from '../data/database.js';
+
 const storageConfig = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'images');
@@ -13,20 +15,23 @@ const storageConfig = multer.diskStorage({
 const upload = multer({ storage: storageConfig });
 const userRoutes = express.Router();
 
-userRoutes.get('/', function (req, res) {
-  res.render('profiles');
+userRoutes.get('/', async function (req, res) {
+  const userProfiles = await db().collection('users').find().toArray();
+  res.render('profiles', { userProfiles });
 });
 
 userRoutes.get('/new-user', function (req, res) {
   res.render('new-user');
 });
 
-userRoutes.post('/profiles', upload.single('image'), function (req, res) {
+userRoutes.post('/profiles', upload.single('image'), async function (req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
 
-  console.log(uploadedImageFile);
-  console.log(userData);
+  await db().collection('users').insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path,
+  });
 
   res.redirect('/');
 });
