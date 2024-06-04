95c7c4569096ba23b99d26585b2937679c572c71
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-06-04 23:04:38 +0900

    header modify & logo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(logger);
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/products/assets' , express.static('product-data'));
+app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(checkAuthStatus);
